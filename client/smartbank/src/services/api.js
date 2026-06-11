abb3f07896039e7afb1761f2b22b3a674832473d
Kolayagnesh <Kolayagnesh05@gmail.com>
2026-06-11 12:25:07 +0530

    Backend deployed and working correctly

--- client/smartbank/src/services/api.js
+++ client/smartbank/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://16.171.36.250:8080/api',
 })
 
 api.interceptors.request.use((config) => {
