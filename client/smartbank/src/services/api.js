168f3a4db20c792e0ac8798159507370dc9455d8
Kolayagnesh <Kolayagnesh05@gmail.com>
2026-06-18 13:13:30 +0530

    Gemini react

--- client/smartbank/src/services/api.js
+++ client/smartbank/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://16.171.36.250:8080/api',
+    baseURL: "/api"
 })
-
 api.interceptors.request.use((config) => {
   const publicRoutes = [
     '/auth/login',
